8ca2dd2245ba68827604224a61f667b568b7d44e
Pietro <dart.murtas@yahoo.it>
2023-01-07 19:19:02 +0100

    Reading real data working with no errors, data is correctly distributed only if the number of the proceses is a multiple fo the data

--- reading_data/reading_ssh.sh
+++ reading_data/reading_ssh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=3:mem=2gb
+#PBS -l select=1:ncpus=5:mem=2gb
 
 # set max execution time
 #PBS -l walltime=0:10:00
@@ -11,4 +11,4 @@
 cd ${PBS_O_WORKDIR}
 mpicc -std=c99 -g -Wall -I /apps/netCDF4.7.0--gcc-9.1.0/include -L /apps/netCDF4.7.0--gcc-9.1.0/lib -lnetcdf -o reading_ssh.out reading_ssh.c -lm
 module load mpich-3.2
-mpirun.actual -n 4 $(pwd)/reading_ssh.out
+mpirun.actual -n 5 $(pwd)/reading_ssh.out
