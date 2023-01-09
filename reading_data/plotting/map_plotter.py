7bb35e4f4a72059f2324ab2160ed43cb24edc6b3
Pietro <dart.murtas@yahoo.it>
2023-01-09 18:35:25 +0100

    Addition of first plots

--- reading_data/plotting/map_plotter.py
+++ reading_data/plotting/map_plotter.py
@@ -11,12 +11,10 @@ def loadNC(path):
     return ds.to_dataframe()
 
 
-
-
 def map_plot(df, grid, filename="map.png"):
     df_grid = pd.concat([grid.reset_index(drop=True), df.reset_index(drop=True)], axis=1, ignore_index=False)
 
-    #gdf = geopandas.GeoDataFrame(df_grid, geometry=geopandas.points_from_xy(df_grid.lon, df_grid.lat))
+    # gdf = geopandas.GeoDataFrame(df_grid, geometry=geopandas.points_from_xy(df_grid.lon, df_grid.lat))
     world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
     ax = world.plot(color='white', edgecolor='black')
     # gdf.plot(ax=ax, color='red')
@@ -25,25 +23,25 @@ def map_plot(df, grid, filename="map.png"):
     # geoplot.polyplot(ssh_grid, ax=ax, zorder=1)
 
     print("KDE")
-    start_index = 4_000_000
-    print(df_grid.iloc[start_index:start_index + 3_000_000])
+
     sns.kdeplot(data=df_grid,  # .iloc[start_index:start_index+3_000_000],
                 x='lon',
                 y='lat',
                 fill=True,
                 cmap='coolwarm',
                 alpha=0.3,
-                gridsize=20,
-                levels=50,
+                gridsize=50,
+                levels=20,
                 ax=ax)
 
-    plt.savefig(filename)
+    plt.savefig(filename, dpi=1000)
     plt.show()
+
+
 ssh = loadNC('../map_summarized.nc')
 unod = loadNC('../reading_u/map_summarized.nc')
 mesh = xr.open_dataset("fesom.mesh.diag.nc")
 grid = pd.concat([mesh["lon"].to_dataframe(), mesh["lat"].to_dataframe()], axis=1)
 
-
-#map_plot(ssh, grid, filename="shh_map.png")
+map_plot(ssh, grid, filename="shh_map.png")
 map_plot(unod, grid, filename="unod_map.png")
