f2b95a40c0e21b8b05e0ac28df817316d01b86dc
Pietro <dart.murtas@yahoo.it>
2023-01-09 17:05:25 +0100

    plotting a slice of ssh

--- reading_data/plotting/map_plotter.py
+++ reading_data/plotting/map_plotter.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import xarray as xr
+import geopandas
+import seaborn as sns
+
+import matplotlib.pyplot as plt
 
 
 def loadNC(path):
@@ -7,10 +11,34 @@ def loadNC(path):
     return ds.to_dataframe()
 
 
-print(loadNC('../map_summarized.nc'))
-print(loadNC('../reading_u/map_summarized.nc'))
+ssh = loadNC('../map_summarized.nc')
+unod = loadNC('../reading_u/map_summarized.nc')
 mesh = xr.open_dataset("fesom.mesh.diag.nc")
 grid = pd.concat([mesh["lon"].to_dataframe(), mesh["lat"].to_dataframe()], axis=1)
+
+ssh_grid = pd.concat([grid.reset_index(drop=True), ssh.reset_index(drop=True)], axis=1, ignore_index=False)
+
+gdf = geopandas.GeoDataFrame(ssh_grid, geometry=geopandas.points_from_xy(ssh_grid.lon, ssh_grid.lat))
+world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
+ax = world.plot(color='white', edgecolor='black')
+# gdf.plot(ax=ax, color='red')
+
+# ax = geoplot.kdeplot(ssh_grid.head(1000), shade=True, cmap='Reds', projection=geoplot.crs.AlbersEqualArea())
+# geoplot.polyplot(ssh_grid, ax=ax, zorder=1)
+
+print("KDE")
+sns.kdeplot(data=ssh_grid.head(3000000),
+            x='lon',
+            y='lat',
+            fill=True,
+            cmap='coolwarm',
+            alpha=0.3,
+            gridsize=200,
+            levels=20,
+            ax=ax)
+
+plt.show()
+print(ssh)
 print(grid)
-
-
+print(ssh_grid)
+print(gdf.head())
