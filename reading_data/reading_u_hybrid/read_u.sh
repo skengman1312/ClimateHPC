505ccc2a0014050b423d2389bcfaf7a110cfdf6b
Mostafa-Haggag <haggago.mostafa@gmail.com>
2023-01-22 23:59:32 +0100

    working shit

--- reading_data/reading_u_hybrid/read_u.sh
+++ reading_data/reading_u_hybrid/read_u.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 #PBS -N final_sub
-#PBS -l select=5:ncpus=5:mpiprocs=5:mem=4gb 
-#PBS -l walltime=0:09:00
+#PBS -l select=1:ncpus=25:mpiprocs=5:mem=8gb 
+#PBS -l walltime=0:20:00
 #PBS -j oe
 # set the excution on the short queue
 #PBS -q short_cpuQ
 cd ${PBS_O_WORKDIR}
-module load Intel_parallel_studio_xe2019u1
-module load mpich-3.2.1--intel-xe-2019u1
+# module load Intel_parallel_studio_xe2019u1
+# module load mpich-3.2.1--intel-xe-2019u1
 export I_MPI_DEBUG=5
 export I_MPI_PIN_DOMAIN=omp
 export I_MPI_PIN_ORDER=compact
 export OMP_NUM_THREADS=5
-export KMP_AFFINITY=verbose,compact
-
-mpicc -std=c99 -g -Wall -fopenmp -I /apps/netCDF4.7.0--gcc-9.1.0/include -L /apps/netCDF4.7.0--gcc-9.1.0/lib -lnetcdf -o reading_u.out reading_u.c -lm -mcmodel medium
-mpirun -n 5 $(pwd)/reading_u.out
+export KMP_AFFINITY=compact
+export OMP_PROC_BIND = true
+mpicc -std=c99 -g -Wall -fopenmp -I /apps/netCDF4.7.0--gcc-9.1.0/include -L /apps/netCDF4.7.0--gcc-9.1.0/lib -lnetcdf -o reading_u.out reading_u_2.c -lm 
+mpirun -prepend-rank $(pwd)/reading_u.out
 date
 
 ALLPROC=`cat $PBS_NODEFILE | wc -l`
@@ -23,3 +23,4 @@ echo The total number of nodes is $ALLPROC
 export THIS_HOST=$(hostname)
 echo Each place corresponds to a single $OMP_PLACES on the target machine. 
 echo The host $THIS_HOST
+## -mcmodel medium
--- reading_data/reading_u_hybrid/todo.md
+++ reading_data/reading_u_hybrid/todo.md
@@ -1,6 +1,6 @@
 ## THINGS i need to finish tommorow
 * get threading properly and fix the time. check ur threading properly with an example and introduce the concept of 
-funneling
+funneling DONEEEEEEE FUCKING DONEEEE
 * create different sh file with different conditions
 * create a cleaner version of reading u full set
 * create a communication module idea 
