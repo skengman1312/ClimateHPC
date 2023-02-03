77efb4f245c02463f527a7e61a96e258414e27ef
Pietro <dart.murtas@yahoo.it>
2023-02-03 22:03:45 +0100

    Stupid color now createst the desired communication architecture, to be copied on main file

--- reading_data/reading_ssh_v2/stupid_color.c
+++ reading_data/reading_ssh_v2/stupid_color.c
@@ -17,7 +17,7 @@ int main () {
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    int color = world_rank / 4; // Determine color based on row
+    int color = world_rank / 3; // Determine color based on row
 
     // Split the communicator based on the color and use the
     // original rank for ordering
@@ -28,8 +28,41 @@ int main () {
     MPI_Comm_rank(row_comm, &row_rank);
     MPI_Comm_size(row_comm, &row_size);
 
-    printf("WORLD RANK/SIZE: %d/%d \t ROW RANK/SIZE: %d/%d\n",
-           world_rank, world_size, row_rank, row_size);
+
+    MPI_Comm copy_comm;
+    MPI_Comm_dup(MPI_COMM_WORLD, &copy_comm);
+    MPI_Group world_group;
+    MPI_Comm_group(copy_comm, &world_group);
+
+    int n = 4;
+    const int ranks[4] = {0, 3, 6, 9};
+
+     MPI_Group new_group;
+     MPI_Group_incl(world_group, 4, ranks, &new_group);
+
+    // Create new communicator based on group
+
+     MPI_Comm new_comm;
+     MPI_Comm_create(copy_comm, new_group, &new_comm);
+
+
+
+    // Get the rank and size in the new communicator
+
+
+    int new_rank = -1, new_size = -1;
+    if (MPI_COMM_NULL != new_comm){
+        MPI_Comm_rank(new_comm, &new_rank);
+        MPI_Comm_size(new_comm, &new_size);
+    }
+
+
+
+
+
+
+    printf("WORLD RANK/SIZE: %d/%d \t ROW RANK/SIZE: %d/%d\t NEW RANK/SIZE: %d/%d\t color %d \n",
+           world_rank, world_size, row_rank, row_size,  new_rank, new_size, color);
 
     MPI_Comm_free(&row_comm);
 }
