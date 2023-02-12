b98b1e19e15ffa94310c5762dd398805ceafdb13
Pietro <dart.murtas@yahoo.it>
2023-02-12 20:28:07 +0100

    cleaning

--- reading_data/plotting/tex_plotter.py
+++ reading_data/plotting/tex_plotter.py
@@ -13,16 +13,26 @@ res = pd.DataFrame([[8.932, 7.586, None],
                     [5.002, None, 2.654]
                     ], columns=["2 groups", "3 groups", "4 groups"], index=[6, 8, 9, 10, 12, 15, 18, 20, 24])
 
+
 if __name__ == "__main__":
+    res = pd.read_csv("../../utils/HPC_final.csv", na_values="-", index_col="Numberofprocesses")
+    res.drop(["Serial"], axis = 1, inplace=True)
+    print(res.columns)
     ypoints = np.array([34.635 / i for i in range(2, 27)])
     xpoints = np.array([i for i in range(2, 27)])
+
+    ypoints = res["Expcted"]
+    res.drop(["Expcted"], axis = 1, inplace=True)
+
+    xpoints = [4,6,9,12,16]
+
     # res.plot()
     # print(res)
     plt.xticks(range(5, 26, 2))
     plt.plot(xpoints, ypoints, label = "T(serial)/n processes")
-    plt.xlim(5,25)
-    plt.ylim(0,10)
-    plt.fill_between(x=xpoints, y1= ypoints-0.4, y2= ypoints+0.4, alpha=.1)
+    # plt.xlim(5,25)
+    # plt.ylim(0,10)
+    plt.fill_between(x=xpoints, y1= ypoints-2, y2= ypoints+2, alpha=.1)
     for c in res:
         print(res[c])
         res[c].dropna().plot(marker = ".")
